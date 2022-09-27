53e12a2b7e492f053b7b0eb62fbdbacfc5fae7d5
Krishna <krshnabiswal619@gmail.com>
2022-09-27 23:28:53 +0530

    solved

---
+++ Linkedlist/Remove-duplicate-from-sorted-list.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode *ptr = head;
+        while (ptr->next)
+        {
+            if (ptr->val == ptr->next->val)
+            {
+                ptr->next = ptr->next->next;
+            }
+            else
+            {
+                ptr = ptr->next;
+            }
+        }
+
+        return head;
+    }
+};
