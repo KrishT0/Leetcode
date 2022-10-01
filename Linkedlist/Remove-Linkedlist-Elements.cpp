6da6f91a34222772d1a72b4dfc24ad193766cdd8
Krishna <krshnabiswal619@gmail.com>
2022-10-01 13:13:53 +0530

    RemoveLinkedlistElemnts

---
+++ Linkedlist/Remove-Linkedlist-Elements.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *head, int val)
+    {
+        if (head == NULL)
+            return head;
+
+        while (head && head->val == val)
+        {
+            head = head->next;
+        }
+
+        ListNode *temp = head;
+
+        while (temp && temp->next)
+        {
+            if (temp->next->val == val)
+            {
+
+                temp->next = temp->next->next;
+            }
+            else
+            {
+                temp = temp->next;
+            }
+        }
+
+        return head;
+    }
+};
