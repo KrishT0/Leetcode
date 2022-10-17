8ffc5f9ed83741481e2915fe8e0fb1ddf0dde53f
Krishna <krshnabiswal619@gmail.com>
2022-10-17 07:35:55 +0530

    PangramSentence

---
+++ String/Check-If-The-Sentence-Is-Pangram.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+public:
+    bool checkIfPangram(string sentence)
+    {
+        set<int> s;
+        for (auto &a : sentence) // put in set
+            s.insert(a);
+        return s.size() == 26; // set gives unique result
+    }
+};
