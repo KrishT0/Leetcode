d8c55b3934b89eb2a3297ba7ec00b1b76a182320
Krishna <krshnabiswal619@gmail.com>
2022-10-30 10:01:21 +0530

    PlusOne

---
+++ Array/Plus-One.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int i = digits.size() - 1;
+
+        while (i >= 0)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i] += 1;
+                return digits;
+            }
+            else
+                digits[i--] = 0;
+        }
+        digits.insert(digits.begin(), 1); // to insert 1 at begining of verctor
+        return digits;
+    }
+};
