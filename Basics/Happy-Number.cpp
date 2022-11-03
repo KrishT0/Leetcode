056963192876901d2317e74fad70e3280fef8443
Krishna <krshnabiswal619@gmail.com>
2022-11-03 19:36:26 +0530

    Create Happy-Number.cpp

---
+++ Basics/Happy-Number.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    bool isHappy(int n)
+    {
+        set<int> result;
+        int val, index;
+
+        while (1)
+        {
+            val = 0;
+            while (n)
+            {
+                index = n % 10;
+                val += index * index;
+                n /= 10;
+            }
+            if (val == 1)
+                return true;
+            else if (result.find(val) != result.end())
+                return false;
+
+            result.insert(val);
+            n = val;
+        }
+        return false;
+    }
+};
