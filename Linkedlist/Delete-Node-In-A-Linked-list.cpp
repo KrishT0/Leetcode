05ef28b367a6910d7a36c7d88cdc62e0a46425c9
Krishna <krshnabiswal619@gmail.com>
2022-11-05 23:31:44 +0530

    Update Delete-Node-In-A-Linked-list.cpp

--- Linkedlist/Delete-Node-In-A-Linked-list.cpp
+++ Linkedlist/Delete-Node-In-A-Linked-list.cpp
@@ -3,8 +3,8 @@ class Solution
 public:
     void deleteNode(ListNode *node)
     {
-        ListNode *temp = node->next;  
-        node->val = temp->val;   //just copy the value of next node of the given node which has to be deleted
+        ListNode *temp = node->next;
+        node->val = temp->val; // just copy the value of next node of the given node which has to be deleted
         node->next = node->next->next;
         delete (temp);
     }
