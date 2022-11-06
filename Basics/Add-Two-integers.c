32e0fafec4abc575ed32116876e7303d3dc65458
Krishna <krshnabiswal619@gmail.com>
2022-11-06 19:38:39 +0530

    Update Add-Two-integers.c

--- Basics/Add-Two-integers.c
+++ Basics/Add-Two-integers.c
@@ -1,4 +1,4 @@
-#define HUE int sum(int num1, int num2){
+#define HUE int sum(int num1, int num2){       //funny way just defining the whole code in huehuehue
 #define Hue return num1+num2;
 #define hue }
 
