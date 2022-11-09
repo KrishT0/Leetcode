16d1987ac362c4b8c86691efda6e0c4c22fc8246
Krishna <krshnabiswal619@gmail.com>
2022-11-09 14:07:28 +0530

    Create Move-Zeroes.c

---
+++ Array/Move-Zeroes.c
@@ -0,0 +1,45 @@
+void moveZeroes(int *nums, int n)
+{
+
+    if (n == 0 || n == 1)
+        return;
+    int i, j;
+    i = j = 0;
+    for (int k = 0; k < n; k++)
+    {
+        if (nums[i] == 0)
+        {
+            i++;
+        }
+        else
+        {
+            if (i != j)
+            {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+            i++;
+            j++;
+        }
+    }
+} // brute force
+
+// snowball algo
+void moveZeroes(int *nums, int n)
+{
+    int snowBallSize = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == 0)
+        {
+            snowBallSize++;
+        }
+        else if (snowBallSize > 0)
+        {
+            int t = nums[i];
+            nums[i] = 0;
+            nums[i - snowBallSize] = t;
+        }
+    }
+}
