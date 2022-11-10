cb39850f9986e4d16a6a490cc6146a03e70662ab
Krishna <krshnabiswal619@gmail.com>
2022-11-11 00:38:19 +0530

    Update Remove-Linkedlist-Elements.cpp

--- Linkedlist/Remove-Linkedlist-Elements.cpp
+++ Linkedlist/Remove-Linkedlist-Elements.cpp
@@ -6,7 +6,7 @@ public:
         if (head == NULL)
             return head;
 
-        while (head && head->val == val)
+        while (head && head->val == val) //head value removal
         {
             head = head->next;
         }
