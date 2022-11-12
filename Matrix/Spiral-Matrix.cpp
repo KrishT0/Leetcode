be90cc859521ded915913b34ef1e96f2b583fef5
Krishna <krshnabiswal619@gmail.com>
2022-11-13 00:06:35 +0530

    Update Spiral-Matrix.cpp

--- Matrix/Spiral-Matrix.cpp
+++ Matrix/Spiral-Matrix.cpp
@@ -3,14 +3,14 @@ class Solution
 public:
     vector<int> spiralOrder(vector<vector<int>> &A)
     {
-        int top, bottom, left, right, dir;
-        top = 0;
+        int top, bottom, left, right, dir; //using 4 pointer approach
+        top = 0;                           //top , bottom , left , right used as boundaries
         bottom = A.size() - 1;
         left = 0;
-        right = A[0].size() - 1;
-        dir = 0;
+        right = A[0].size() - 1;          // directions  0 = left to right ; 1 = top to bottom
+        dir = 0;                          // directions  2 = right to left ; 3 = bottom to top 
         int i;
-        vector<int> ans;
+        vector<int> ans;  // solution vector
 
         while (top <= bottom && left <= right)
         {
@@ -38,7 +38,7 @@ public:
                     ans.push_back(A[i][left]);
                 left++;
             }
-            dir = (dir + 1) % 4;
+            dir = (dir + 1) % 4;  // so that doirect should not exceed 3
         }
         return ans;
     }
