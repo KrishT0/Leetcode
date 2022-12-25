858262e0d4c80a6b5dd3927ef87a45fdba55b413
Krishna <krshnabiswal619@gmail.com>
2022-12-25 22:24:59 +0530

    Create Binary-Level-Order-Traversal.cpp

---
+++ Tree/Binary-Level-Order-Traversal.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+     vector<vector<int>> levelOrder(TreeNode *root)
+     {
+          vector<vector<int>> ans;
+          if (root == NULL)
+               return ans;
+
+          queue<TreeNode *> q;
+          q.emplace(root);
+
+          while (!q.empty())
+          {
+               vector<int> level;
+               int size = q.size();
+               for (int i = 0; i < size; i++)
+               {
+                    TreeNode *node = q.front();
+                    q.pop();
+
+                    if (node->left)
+                         q.emplace(node->left);
+                    if (node->right)
+                         q.emplace(node->right);
+
+                    level.emplace_back(node->val);
+               }
+               ans.emplace_back(level);
+          }
+          return ans;
+     }
+};
