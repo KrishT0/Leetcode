329093627d0296b4bf633f99efcff4df86d60a2d
Krishna <krshnabiswal619@gmail.com>
2023-01-01 21:57:31 +0530

    Update Balanced-Binary-Tree.cpp

--- Tree/Balanced-Binary-Tree.cpp
+++ Tree/Balanced-Binary-Tree.cpp
@@ -25,3 +25,35 @@ public:
           return height(root) != -1;
      }
 };
+
+// brute-force
+class Solution
+{
+public:
+     int height(TreeNode *root)
+     {
+          if (root == NULL)
+               return 0;
+          int l = height(root->left), r = height(root->right);
+          return 1 + max(l, r);
+     }
+
+     bool isBalanced(TreeNode *root)
+     {
+          if (root == NULL)
+               return true;
+
+          int l = height(root->left);
+          int r = height(root->right);
+
+          if (abs(l - r) > 1)
+               return false;
+
+          bool left = isBalanced(root->left);
+          bool right = isBalanced(root->right);
+
+          if (!left || !right)
+               return false;
+          return true;
+     }
+};
