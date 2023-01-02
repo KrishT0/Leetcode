4b9611d662531e34a02ee99607698dd07273c658
Krishna <krshnabiswal619@gmail.com>
2023-01-02 20:04:39 +0530

    Update Balanced-Binary-Tree.cpp

--- Tree/Balanced-Binary-Tree.cpp
+++ Tree/Balanced-Binary-Tree.cpp
@@ -1,31 +1,24 @@
 class Solution
 {
 public:
-     int height(TreeNode *root)
+     int check(TreeNode *root)
      {
           if (root == NULL)
                return 0;
-          int l = height(root->left), r = height(root->right);
+          int l = check(root->left);
+          if (l == -1)
+               return -1;
+          int r = check(root->right);
+          if (r == -1)
+               return -1;
+          if (abs(l - r) > 1)
+               return -1;
           return 1 + max(l, r);
      }
 
      bool isBalanced(TreeNode *root)
      {
-          if (root == NULL)
-               return true;
-
-          int l = height(root->left);
-          int r = height(root->right);
-
-          if (abs(l - r) > 1)
-               return false;
-
-          bool left = isBalanced(root->left);
-          bool right = isBalanced(root->right);
-
-          if (!left || !right)
-               return false;
-          return true;
+          return check(root) != -1;
      }
 };
 
