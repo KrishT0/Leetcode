052bddbcb16429fa9ccaf627bec63928b82680a4
Krishna Biswal <krshnabiswal619@gmail.com>
2023-02-25 16:32:28 +0530

    Update Length of Last word.java

--- String/Length of Last word.java
+++ String/Length of Last word.java
@@ -2,12 +2,9 @@ class Solution {
     public int lengthOfLastWord(String s) {
      Stack<String> stack = new Stack<>();
         String[]  words = s.split(" ");
-
         for(String n : words){
             stack.push(n);
         }
-
         return stack.isEmpty() ? 0 : stack.peek().length();
-
     }
 }
