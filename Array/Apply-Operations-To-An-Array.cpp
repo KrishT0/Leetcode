1572960c03c36c90d9217bd2b0f0727bdce23aa8
Krishna <krshnabiswal619@gmail.com>
2023-03-25 12:53:21 +0530

    Modify Apply-operations-To-An-Array.cpp

--- Array/Apply-Operations-To-An-Array.cpp
+++ Array/Apply-Operations-To-An-Array.cpp
@@ -1,55 +1,34 @@
-// leetcode
-//space complexity : O(n)
-//time complexity : O(n)
-
-
-
-// not working in leetcode but working in vs code
-//space complexity : O(1)
-//time complexity : O(n)
-#include <iostream>
-#include <vector>
-#include <stdlib.h>
-using namespace std;
-void snowball(vector<int> &nums)
+class Solution
 {
-     int snowBallSize = 0;
-     for (int i = 0; i < nums.size(); i++)
+public:
+     void snowball(vector<int> &nums)
      {
-          if (nums[i] == 0)
-               snowBallSize++;
-          else if (snowBallSize > 0)
+          int snowBallSize = 0;
+          for (int i = 0; i < nums.size(); i++)
           {
-               int t = nums[i];
-               nums[i] = 0;
-               nums[i - snowBallSize] = t;
+               if (nums[i] == 0)
+                    snowBallSize++;
+               else if (snowBallSize > 0)
+               {
+                    int t = nums[i];
+                    nums[i] = 0;
+                    nums[i - snowBallSize] = t;
+               }
           }
      }
-}
 
-vector<int> applyOperations(vector<int> &nums)
-{
-     for (int i = 0; i < nums.size(); i++)
+     vector<int> applyOperations(vector<int> &nums)
      {
-          if (nums[i] == nums[i + 1])
+          for (int i = 0; i < nums.size() - 1; i++)
           {
-               nums[i] = 2 * nums[i];
-               nums[i + 1] = 0;
-               i++;
+               if (nums[i] == nums[i + 1])
+               {
+                    nums[i] = 2 * nums[i];
+                    nums[i + 1] = 0;
+                    i++;
+               }
           }
+          snowball(nums);
+          return nums;
      }
-     snowball(nums);
-     return nums;
-}
-
-int main()
-{
-     vector<int> a1 = {1, 2, 2, 1, 1, 0};
-     applyOperations(a1);
-     for (int i = 0; i < 6; i++)
-     {
-          cout << a1[i];
-     }
-     return 0;
-}
-
+};
