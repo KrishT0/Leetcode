0b597ff25bea784d98788b44e6c259c83720a7f8
Krishna <krshnabiswal619@gmail.com>
2023-03-27 19:50:24 +0530

    Update Array-Partition.py

--- Array/Array-Partition.py
+++ Array/Array-Partition.py
@@ -1,13 +1,7 @@
-
-
 class Solution:
-    def arrayPairSum(self, nums:list[int]) -> int:
+    def arrayPairSum(self, nums: list[int]) -> int:
         nums.sort()
         sum_ = 0
-        for i in range(0,len(nums),2):
+        for i in range(0, len(nums), 2):
             sum_ += nums[i]
         return sum_
-
-    
-s=Solution()
-print(s.arrayPairSum([2,7,1,9,4,6]))
